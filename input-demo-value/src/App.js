d32d27d252a87094c7f3d423b3c52beb91997bff
fitzgallo <fitzgerald_gallo@rcc.edu.ph>
2024-09-14 12:20:10 +0800

    Reactjs

---
+++ input-demo-value/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import InputComponents from './InputComponents'; 
+import Addnumbers from './Addnumbers';
+
+function App() {
+  return (
+    <div className="App">
+      <InputComponents/>
+      <Addnumbers/>
+    </div>
+  );
+}
+
+export default App;
---
+++ input-demo-value/src/InputComponents.js
@@ -0,0 +1,35 @@
+import React,{Component}from 'react';
+
+class InputComponents extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+        inputValue: '',
+        };
+    }
+
+
+    handleInputChange = (event) => {
+        this.setState({
+            inputValue: event.target.value,
+        });
+    };
+
+    render(){
+        return(
+            <div>
+                <h2>Input Box Value</h2>
+                <input
+                type="text"
+                placeholder="Enter something..."
+                value={this.state.inputValue}
+                onChange={this.handleInputChange}
+                />
+                <p>You Typed:{this.state.inputValue}</p>
+                </div>
+  
+        );
+    }
+}
+
+export default InputComponents;
